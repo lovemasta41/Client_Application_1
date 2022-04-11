dad15c1618013729cab5e083b6006d3dd2276dc5
geops-autonomics <geops-autonomics@hcl.com>
2022-04-11 09:05:56 +0000

    updating yaml file

--- terraform.tf
+++ terraform.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 
   }
   #use_msi         = true
-  subscription_id = var.subscription_id
+  #subscription_id = var.subscription_id
   #client_id = var.client_id
   #client_secret = var.client_secret
   #tenant_id = var.tenant_id
